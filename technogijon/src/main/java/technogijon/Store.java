8d9ffcb4c241b2d4aa5dc21395866169917b8ae3
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-23 22:20:26 +0100

    method for name owner and taxId

--- technogijon/src/main/java/technogijon/Store.java
+++ technogijon/src/main/java/technogijon/Store.java
@@ -4,31 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
+    private String name;
+    private String owner;
+    private String taxId;
     private List<Computer> computers;
 
-    public Store() {
+    // Constructor
+    public Store(String name, String owner, String taxId) {
+        this.name = name;
+        this.owner = owner;
+        this.taxId = taxId;
         this.computers = new ArrayList<>();
     }
 
-    public void addComputer(Computer computer) {
-        computers.add(computer);  
+    public String getName() {
+        return name;
     }
 
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getTaxId() {
+        return taxId; }
+
     public List<Computer> listComputers() {
-        return computers;  
+        return computers;
+    }
+
+    // Métodos de la tienda
+    public void addComputer(Computer computer) {
+        computers.add(computer);
     }
 
     public void removeComputer(String brand) {
-        computers.removeIf(computer -> computer.getBrand().equals(brand)); 
+        computers.removeIf(computer -> computer.getBrand().equals(brand));
     }
 
     public Computer findComputerByBrand(String brand) {
         for (Computer computer : computers) {
             if (computer.getBrand().equals(brand)) {
-                return computer;  
+                return computer;
             }
         }
-        return null;  
+        return null;
     }
-    
 }
